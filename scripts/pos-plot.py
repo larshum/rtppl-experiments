1ca24523800e88a270236e21b895ca8bf4d6b41f
larshum <larshum@kth.se>
2022-10-26 23:01:03 +0200

    Fix axes of position plotter

--- scripts/pos-plot.py
+++ scripts/pos-plot.py
@@ -1,8 +1,16 @@
+# Prevent matplotlib from launching window on every run
+import matplotlib as mpl
+mpl.use('Agg')
+
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import os
 import sys
+
+if not os.path.exists("plots/"):
+    os.mkdir("plots")
 
 if len(sys.argv) > 1:
     # Use a heatmap with 1cm precision
@@ -17,15 +25,17 @@ if len(sys.argv) > 1:
     if len(inputs) % 1000 == 0:
         i = 0
         while i < len(inputs):
+            fig, axs = plt.subplots(1)
             data = np.zeros([rows, cols])
             for line in inputs[i:i+1000]:
                 x, y, _ = line.split(" ")
                 x = int(100 * float(x))
                 y = int(100 * float(y))
-                data[x][y] += 1
-            plt.imshow(data)
-            plt.colorbar()
-            plt.show()
+                data[y][x] += 1
+            axs.set_xlabel("x")
+            axs.set_ylabel("y")
+            axs.imshow(data)
+            fig.savefig(f"plots/{int(i/1000)}.png")
             i = i + 1000
     else:
         print("Invalid number of input lines")
