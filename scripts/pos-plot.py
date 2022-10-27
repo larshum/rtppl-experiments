5c87c5ccd12e9f99d558f7f769d7de4857c5c134
larshum <larshum@kth.se>
2022-10-27 10:36:22 +0200

    Prevent overwriting previous image outputs

--- scripts/pos-plot.py
+++ scripts/pos-plot.py
@@ -3,6 +3,7 @@ import matplotlib as mpl
 mpl.use('Agg')
 
 from PIL import Image
+from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -13,6 +14,12 @@ if not os.path.exists("plots/"):
     os.mkdir("plots")
 
 if len(sys.argv) > 1:
+    # Make a new directory based on current timestamp, so that we don't have to
+    # overwrite old plots.
+    now = datetime.now()
+    target = f'plots/{now.strftime("%Y%m%d-%H%M%S")}'
+    os.mkdir(target)
+
     # Use a heatmap with 1cm precision
     roomFile = sys.argv[1]
     im = Image.open(roomFile)
@@ -22,7 +29,7 @@ if len(sys.argv) > 1:
     axs0.imshow(im)
     axs0.set_xlabel("x")
     axs0.set_ylabel("y")
-    fig0.savefig(f"plots/0.png")
+    fig0.savefig(f"{target}/0.png")
 
     # Read the printed distribution from stdin and produce one image per 1000
     # outputs (corresponding to one inference iteration).
@@ -41,7 +48,7 @@ if len(sys.argv) > 1:
             axs.imshow(im, alpha=0.5)
             axs.set_xlabel("x")
             axs.set_ylabel("y")
-            fig.savefig(f"plots/{int(i/1000)+1}.png")
+            fig.savefig(f"{target}/{int(i/1000)+1}.png")
             i = i + 1000
     else:
         print("Invalid number of input lines")
