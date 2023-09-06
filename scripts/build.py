8b92b16a49be9e07c692510c75f2f57fea47a550
Lars Hummelgren <larshum@kth.se>
2023-09-06 15:04:59 +0200

    Update build script to make it more flexible

--- scripts/build.py
+++ scripts/build.py
@@ -9,7 +9,16 @@ def write_line(f, targets):
 
 path = sys.argv[1]
 os.chdir(path)
-p = subprocess.run(["rtppl", "posmodel.rpl"] + sys.argv[2:])
-if p.returncode != 0:
-    print("RTPPL compilation failed")
+rtppl_files = [f for f in os.listdir(".") if f.endswith(".rpl")]
+if len(rtppl_files) == 0:
+    print("Directory contains no RTPPL files")
+    exit(1)
+elif len(rtppl_files) == 1:
+    rtppl_file = rtppl_files[0]
+    p = subprocess.run(["rtppl", rtppl_file] + sys.argv[2:])
+    if p.returncode != 0:
+        print(f"RTPPL compilation failed for file {rtppl_file}")
+        exit(1)
+else:
+    print("Directory contains multiple RTPPL files, expected one")
     exit(1)
