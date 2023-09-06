d27effd94438e63321454482ab57c6189c0495ea
Lars Hummelgren <larshum@kth.se>
2023-09-06 14:04:22 +0200

    Remove seemingly unused code

--- scripts/build.py
+++ scripts/build.py
@@ -13,18 +13,3 @@ p = subprocess.run(["rtppl", "posmodel.rpl"] + sys.argv[2:])
 if p.returncode != 0:
     print("RTPPL compilation failed")
     exit(1)
-
-nw = network.read_network("network.json")
-sensors = nw["sensor_outs"]
-with open("sa.txt", "w+") as f:
-    write_line(f, sensors["frontLeft"])
-    write_line(f, sensors["frontRight"])
-    write_line(f, sensors["rearLeft"])
-    write_line(f, sensors["rearRight"])
-    write_line(f, sensors["sideLeft"])
-    write_line(f, sensors["sideRight"])
-    write_line(f, sensors["speedLeft"])
-    write_line(f, sensors["speedRight"])
-    write_line(f, sensors["steeringAngle"])
-    write_line(f, nw["actuator_ins"]["brake"])
-
