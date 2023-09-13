e709f059c05b470652b7c3e60f6795ad07daa4e6
Lars Hummelgren <larshum@kth.se>
2023-09-13 18:35:51 +0200

    Update collected data plotter to use scatter plots

--- scripts/plot-collected.py
+++ scripts/plot-collected.py
@@ -26,31 +26,18 @@ def check_overrun(obs):
         if o[2] > 0:
             print(f"Task overran by {o[2]} ns")
 
-def wcet_per_particle_count(obs):
-    wcet = {}
-    for o in obs:
-        exec_time = o[0]
-        p = o[1]
-        if p in wcet:
-            if exec_time > wcet[p]:
-                wcet[p] = exec_time
-        else:
-            wcet[p] = exec_time
-    return wcet
+def part_exec_pairs(obs):
+    return zip(*[(o[1], o[0]) for o in obs])
 
 now = datetime.now()
 target = f'plots/{now.strftime("%Y%m%d-%H%M%S")}'
 os.mkdir(target)
 for arg in sys.argv[1:]:
+    print(arg)
     obs = read_collected(arg)
     check_overrun(obs)
-    wcet = wcet_per_particle_count(obs)
-    if len(wcet) > 0:
-        fig, axs0 = plt.subplots(1)
-        ordered = sorted(wcet.items())
-        x, y = zip(*ordered)
-        axs0.plot(x, y)
-        axs0.set_xlabel("#particles")
-        axs0.set_ylabel("worst-case execution time")
-        name, _ = os.path.splitext(os.path.basename(arg))
-        fig.savefig(f"{target}/{name}.png")
+    fig, axs0 = plt.subplots(1)
+    name, _ = os.path.splitext(os.path.basename(arg))
+    x, y = part_exec_pairs(obs)
+    axs0.scatter(x, y)
+    fig.savefig(f"{target}/{name}.png")
