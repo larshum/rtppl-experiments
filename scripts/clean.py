4345c571e80d0c20d9199c984ed6db8f05c39080
Lars Hummelgren <larshum@kth.se>
2023-10-04 12:14:49 +0200

    Update and clarify behavior of clean script

--- scripts/clean.py
+++ scripts/clean.py
@@ -7,19 +7,39 @@ import sys
 
 import network
 
+nwfile = "network.json"
+
 def try_remove(f):
     try:
         os.remove(f)
     except FileNotFoundError:
         pass
 
-def try_clear(f):
-    if os.path.isfile(f):
-        try:
-            with open(f, "w"):
-                pass
-        except FileNotFoundError:
-            pass
+def keep_file(clear_all_temp, nw, f):
+    task_ids = []
+
+    # We always want to keep source files
+    if f.endswith(".rpl"):
+        return True
+
+    # If the '-a' flag is set, we remove all non-source files
+    if clear_all_temp:
+        return False
+
+    # If it is a binary file, or the system specification file, we keep it to
+    # avoid recompilation.
+    bins = set([t["id"] for t in nw["tasks"]])
+    if f in bins or f == nwfile:
+        return True
+
+    # If it is a task configuration file, we keep it to avoid having
+    # re-configuring the number of particles to use in the task.
+    configs = set([f"{t['id']}.config" for t in nw["tasks"]])
+    if f in configs:
+        return True
+
+    # Otherwise, we don't need to keep it
+    return False
 
 p = argparse.ArgumentParser()
 p.add_argument("-p", "--path", action="store", required=True)
@@ -28,12 +48,8 @@ args = p.parse_args()
 
 path = args.path
 os.chdir(path)
-nwfile = f"network.json"
-data = network.read_network(nwfile)
+nw = network.read_network(nwfile)
 
-# If the '-a' flag is set, we remove all files except for the original source
-# files. Otherwise, we also keep the configuration files.
 for f in os.listdir("."):
-    if not f.endswith(".rpl"):
-        if not args.a and not f.endswith(".config"):
-            try_remove(f)
+    if not keep_file(args.a, nw, f):
+        try_remove(f)
