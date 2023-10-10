91234adbf83131973df8c341e23bcc06e3ac03cf
Lars Hummelgren <larshum@kth.se>
2023-10-10 16:43:51 +0200

    Compute distance to true coords in print-expected

--- scripts/print-expected.py
+++ scripts/print-expected.py
@@ -1,6 +1,7 @@
 import dist
 import os
 import sys
+from math import sqrt
 
 def compute_expected(v):
     x = 0.0
@@ -14,8 +15,18 @@ def compute_expected(v):
 
 posFile = "pos-posEst"
 path = sys.argv[1]
+if len(sys.argv) == 4:
+    true_x = float(sys.argv[2])
+    true_y = float(sys.argv[3])
+else:
+    true_x = None
+    true_y = None
 if os.path.isfile(path):
     pos_dists = sorted(dist.read_dists(path, 3))
     for ts, v in pos_dists:
         x, y, d = compute_expected(v)
-        print(f"{ts}| {x} {y} {d} ({len(v)})")
+        print(f"{ts} | {x} {y} {d} ({len(v)})")
+if true_x is not None:
+    _, lastv = pos_dists[-1]
+    x, y, _ = compute_expected(lastv)
+    print(f"Error: {sqrt((x-true_x)**2+(y-true_y)**2)}")
