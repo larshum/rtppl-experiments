81a6f66ec5830d96b80dd694946f71de1bf06b1a
Lars Hummelgren <larshum@kth.se>
2024-05-03 18:22:41 +0200

    Add in-progress scripts for running benchmarks

---
+++ run-automatic-configuration.sh
@@ -0,0 +1,113 @@
+BUILD_PATH=positioning
+MAP_ID=maps/corridor
+TRAIN_PATH=measurements/train_data
+TEST_PATH=measurements/test_data
+TRUE_POS_PATH=${TEST_PATH}/true.txt
+
+# Sanity check: ensure data for testing and training exists
+if [ ! -d ${TRAIN_PATH} ]; then
+  echo "Training data measurements directory not found (${TRAIN_PATH})"
+  exit 1
+fi
+if [ ! -d ${TEST_PATH} ]; then
+  echo "Test data measurements directory not found (${TEST_PATH})"
+  exit 1
+fi
+
+# Create the textual map representation used by the ProbTime program if it does
+# not already exist.
+if [ ! -f ${MAP_ID}.txt ]; then
+  python3 scripts/map-conv.py ${MAP_ID}.png > ${MAP_ID}.txt
+fi
+
+# Rebuild the ProbTime program, removing any previous files in the build path
+# if it had already been compiled.
+if [ -f ${BUILD_PATH}/system.json ]; then
+  python3 scripts/clean.py -p ${BUILD_PATH} -a
+fi
+python3 scripts/build.py ${BUILD_PATH}
+
+echo "###########################"
+echo "# AUTOMATIC CONFIGURATION #"
+echo "###########################"
+
+# This experiment tests the outcome of the automatic configuration when varying
+# the ratio of importance assigned to the pos and braking tasks. We start with
+# both tasks having importance one, and double the importance of one task until
+# we reach a point where we cannot find a schedulable configuration satisfying
+# the given constraints. We perform this for both tasks and using both
+# approaches to fairness.
+#
+# For each measurement, we collect the resulting number of particles per task
+# (pos and braking) and the configuration time (in terms of the number of
+# iterations).
+
+# 1. Set the task to core mapping such that the tasks that perform inference
+#    are on the same core, and the other tasks are on another core.
+printf "core\npos 1\nbraking 1\nspeed 2\ndistance_SL 2\ndistance_SR 2\ndistance_FC 2\n" | python3 scripts/set-property.py ${BUILD_PATH}
+
+CONFIGURATIONS=("particle" "execution-time")
+CONFIGURATION_FLAGS=("--particle-fairness" "--execution-time-fairness")
+TASKS=("pos" "budget")
+RUNNER="sudo python3 scripts/runner.py -p ${BUILD_PATH} -m ${MAP_ID}.txt -r ${TRAIN_PATH}"
+for i in $(seq 0 1)
+do
+  FAIRNESS=${CONFIGURATIONS[i]}
+  CONFIG_FLAGS=${CONFIGURATION_FLAGS[i]}
+  echo "Evaluating ${FAIRNESS} fairness"
+  for j in $(seq 0 1)
+  do
+    TASK=${TASKS[j]}
+    OUTDIR=measurements/${FAIRNESS}
+
+    mkdir -p ${OUTDIR}
+
+    echo "Increasing the importance of the ${TASK} task"
+
+    # Overwrite the initial importance values in the system.json file such that
+    # the pos and braking tasks are assigned importance one and the other tasks
+    # have importance zero (as they perform no inference).
+    printf "importance\npos 1\nbraking 1\nspeed 0\ndistance_SL 0\ndistance_SR 0\ndistance_FC 0\n" | python3 scripts/set-property.py ${BUILD_PATH}
+
+    RES=0
+    PPOS=1
+    PBRAKING=1
+    while :
+    do
+      # We double at the end if j = 0 and at the start if j = 1, so that we
+      # only run the 1 to 1 ratio configuration once.
+      if [ "$j" -eq "1" ]; then
+        PBRAKING=$((2 * PBRAKING))
+      fi
+
+      # Set the importance values of tasks in the configuration file.
+      printf "importance\npos ${PPOS}\nbraking ${PBRAKING}\nspeed 0\ndistance_SL 0\ndistance_SR 0\ndistance_FC 0\n" | python3 scripts/set-property.py ${BUILD_PATH}
+
+      LOG_OUTPUT=${OUTDIR}/${PPOS}-${PBRAKING}-log.txt
+
+      rtppl-configure --repetitions 1 --safety-margin 0.01 --path ${BUILD_PATH} --runner "${RUNNER}" ${CONFIG_FLAGS} > ${LOG_OUTPUT}
+
+      # If the configuration failed, e.g., because the ratio between the
+      # importance of the tasks is too large, we stop the experiment here.
+      if [ "$?" -ne "0" ]; then
+        printf "\nMax ratio: ${PPOS} ${PBRAKING}\n"
+        break
+      else
+        printf "."
+      fi
+
+      # Copy the resulting configuration file
+      cp ${BUILD_PATH}/system.json ${OUTDIR}/system-${PPOS}-${PBRAKING}.json
+
+      if [ "${j}" -eq "0" ]; then
+        PPOS=$((2 * PPOS))
+      fi
+    done
+  done
+done
+
+echo "#####################"
+echo "# RESULT PROCESSING #"
+echo "#####################"
+
+echo "TODO"
---
+++ run-inference-accuracy.sh
@@ -0,0 +1,100 @@
+BUILD_PATH=positioning
+MAP_ID=maps/corridor
+TRAIN_PATH=measurements/train_data
+TEST_PATH=measurements/test_data
+TRUE_POS_PATH=${TEST_PATH}/true.txt
+
+# Sanity check: ensure data for testing and training exists
+if [ ! -d ${TRAIN_PATH} ]; then
+  echo "Training data measurements directory not found (${TRAIN_PATH})"
+  exit 1
+fi
+if [ ! -d ${TEST_PATH} ]; then
+  echo "Test data measurements directory not found (${TEST_PATH})"
+  exit 1
+fi
+
+# Create the textual map representation used by the ProbTime program if it does
+# not already exist.
+if [ ! -f ${MAP_ID}.txt ]; then
+  python3 scripts/map-conv.py ${MAP_ID}.png > ${MAP_ID}.txt
+fi
+
+# Rebuild the ProbTime program, removing any previous files in the build path
+# if it had already been compiled.
+if [ -f ${BUILD_PATH}/system.json ]; then
+  python3 scripts/clean.py -p ${BUILD_PATH} -a
+fi
+python3 scripts/build.py ${BUILD_PATH}
+
+echo "###############################"
+echo "# POSITION INFERENCE ACCURACY #"
+echo "###############################"
+
+# These tests fix the particle counts of all tasks while varying the number of
+# particles used in the positioning task. The goal is to show that more
+# particles lead to a better accuracy but that it takes longer. Also, it shows
+# the dependencies among tasks.
+
+# We specify the number of particles to use in the positioning task (all other
+# tasks performing inference run 1000 particles). When running many particles,
+# we need to use a slowdown factor to ensure we have time to run the inference.
+# This factor may need to be adjusted based on the computer on which we run the
+# simulation.
+PC=(100 1000 10000 100000)
+SLOWDOWN=(1 1 1 8)
+
+# 1. Set the task core mapping, allocating the tasks that perform inference
+#    (pos and braking) to separate cores.
+printf "core\npos 1\nbraking 2\nspeed 3\ndistance_SL 3\ndistance_SR 3\ndistance_FC 3\n" | python3 scripts/set-property.py ${BUILD_PATH}
+
+# 2. Update the configuration of the braking task to use 1000 particles.
+printf "particles\nbraking 1000\n" | python3 scripts/set-property.py ${BUILD_PATH}
+
+# 3. Update the budgets of the pos and braking tasks to ensure both have
+# sufficient time but neither (should) overrun their budgets.
+printf "budget\npos 500000000\nbraking 250000000\n" | python3 scripts/set-property.py ${BUILD_PATH}
+
+# 4. Run the simulation, varying the number of particles in each step.
+for i in $(seq 0 3)
+do
+  # Set the number of particles to use in the positioning task for this
+  # measurement.
+  printf "particles\npos ${PC[i]}\n" | python3 scripts/set-property.py ${BUILD_PATH}
+
+  mkdir -p measurements/particles-${i}
+
+  # Repeat the simulation multiple times per particle configuration.
+  for j in $(seq 1 10)
+  do
+    echo "Iteration ${j} (${PC[i]} particles)"
+
+    OUT_DIR=measurements/particles-${i}/${j}
+    mkdir -p ${OUT_DIR}
+
+    # Run the simulation on the test data using the given configuration
+    time sudo python3 scripts/runner.py -p ${BUILD_PATH} -m ${MAP_ID}.txt -r ${TEST_PATH} --record --slowdown ${SLOWDOWN[i]}
+
+    # Copy all files in the build directory to the output folder. We want to
+    # keep most files for sanity checking, but the executables are not
+    # important so we remove them (they also take up a lot of space).
+    cp ${BUILD_PATH}/* ${OUT_DIR}
+    find ${OUT_DIR} -type f -executable | xargs rm
+  done
+
+  for j in $(seq 1 10)
+  do
+    OUT_DIR=measurements/particles-${i}/${j}
+
+    python3 scripts/print-expected.py ${OUT_DIR}/posDebug-actuator ${TRUE_POS_PATH} >> measurements/particles-${i}/accuracy.txt
+  done
+done
+
+echo "#####################"
+echo "# RESULT PROCESSING #"
+echo "#####################"
+
+# Two plots side-by-side: on the left-hand side, a box plot showing how the
+# error decreases as we increase the number of particles in the pos task. On
+# the right-hand side plot, we show the execution times.
+python3 scripts/plot-box.py "${TEST_PATH}/true.txt"
