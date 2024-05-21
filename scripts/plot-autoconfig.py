d83cc9101650292623cb8f5e71d7e0827aa139e7
Lars Hummelgren <larshum@kth.se>
2024-05-21 22:44:58 +0200

    Set dimensions of auto-config plot

--- scripts/plot-autoconfig.py
+++ scripts/plot-autoconfig.py
@@ -112,4 +112,6 @@ axs.set_yscale("log", base=2)
 axs.set_xticks([2**i for i in range(-10, 12, 2)])
 axs.set_yticks([2**i for i in range(-10, 12, 2)])
 axs.legend(loc="upper left")
+
+fig.set_size_inches(8, 4)
 fig.savefig("auto-config.pdf",bbox_inches="tight")
